72688ac25e01fb687ab794911c9932a286eba4df
Geovanny Cordova <geovanny.cordova@willdom.com>
2022-09-30 15:14:34 -0500

    test okay in test

--- test/mutants.test.js
+++ test/mutants.test.js
@@ -8,18 +8,6 @@ beforeAll(async () => await db.connect());
 afterEach(async () => await db.clear());
 afterAll(async () => await db.close());
 
-// test("POST /api/mutant, Should_Reclute_Mutant", (done) => {
-//   agent
-//     .post("/api/mutant")
-//     .send({
-//       dna: "['aerwer','berwerw','erwerc','werwerd']",
-//     })
-//     .expect(200)
-//     .then((res) => {
-//       done();
-//     });
-// });
-
 describe("POST /api/mutant", () => {
   test("Should Verify Mutant OK", async () => {
     const response = await agent.post("/api/mutant").send({
